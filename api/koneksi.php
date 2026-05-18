117f29c87791766d01dd645a053ad4a59dc41e29
tamaabdy25-crypto <tamaabdy25@gmail.com>
2026-05-18 14:44:04 +0700

    Implement MySQL-like functions for PostgreSQL
    Added wrapper functions for PostgreSQL compatibility with MySQL functions.

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,9 +1,25 @@
 <?php
 $db_url = getenv('DATABASE_URL');
-// Supabase pake PostgreSQL, jadi kita pake fungsi pg_connect
 $conn = pg_connect($db_url);
 
-if (!$conn) {
-    die("Koneksi gagal bosku: " . pg_last_error());
+// FUNGSI PENERJEMAH (Wrapper)
+function mysqli_query($conn, $query) {
+    return pg_query($conn, $query);
+}
+
+function mysqli_fetch_assoc($result) {
+    return pg_fetch_assoc($result);
+}
+
+function mysqli_num_rows($result) {
+    return pg_num_rows($result);
+}
+
+function mysqli_real_escape_string($conn, $str) {
+    return pg_escape_string($conn, $str);
+}
+
+function mysqli_error($conn) {
+    return pg_last_error($conn);
 }
 ?>
