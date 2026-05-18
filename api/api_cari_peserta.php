31dfa96072c43fdf23157c843115afec1ff6b43a
tamaabdy25-crypto <tamaabdy25@gmail.com>
2026-05-18 16:22:15 +0700

    Update api_cari_peserta.php

--- api/api_cari_peserta.php
+++ api/api_cari_peserta.php
@@ -1,16 +1,17 @@
 <?php
 include 'koneksi.php';
 
-$search = $_GET['q'] ?? '';
+// Ditambah escape string biar aman dari karakter aneh/hacker
+$search = pg_escape_string($conn, $_GET['q'] ?? '');
 
 // Cari nama lengkap karyawan atau email dari tabel users
-$query = mysqli_query($conn, "SELECT id, nama_lengkap, email FROM users 
+$query = pg_query($conn, "SELECT id, nama_lengkap, email FROM users 
                               WHERE nama_lengkap LIKE '%$search%' 
                               OR email LIKE '%$search%' 
                               LIMIT 10");
 
 $data = [];
-while($row = mysqli_fetch_assoc($query)) {
+while($row = pg_fetch_assoc($query)) {
     $data[] = [
         'id'   => $row['id'], // ID ini yang nanti disimpan ke database
         'text' => $row['nama_lengkap'] . " (" . $row['email'] . ")" // Teks ini yang muncul di layar
