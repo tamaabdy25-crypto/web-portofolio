e7a047ad5e44a43f1637890073ae5c80db9d5eab
tamaabdy25-crypto <tamaabdy25@gmail.com>
2026-05-19 08:55:29 +0700

    Update get_user_name.php

--- api/get_user_name.php
+++ api/get_user_name.php
@@ -2,14 +2,14 @@
 include 'koneksi.php';
 
 if (isset($_GET['no_induk'])) {
-    $no_induk = mysqli_real_escape_string($conn, $_GET['no_induk']);
+    $no_induk = pg_escape_string($conn, $_GET['no_induk']);
     
     // Ganti 'users' dengan nama tabel kamu, dan 'nomor_induk'/'nama_lengkap' sesuai kolommu
     $sql = "SELECT nama_lengkap FROM users WHERE nomor_induk = '$no_induk' LIMIT 1";
-    $res = mysqli_query($conn, $sql);
+    $res = pg_query($conn, $sql);
 
-    if (mysqli_num_rows($res) > 0) {
-        $row = mysqli_fetch_assoc($res);
+    if (pg_num_rows($res) > 0) {
+        $row = pg_fetch_assoc($res);
         echo $row['nama_lengkap'];
     } else {
         echo "Data tidak ditemukan";
