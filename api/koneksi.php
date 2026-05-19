e357e2f11d34b91536ca267a9de89cd36c93d9fa
tamaabdy25-crypto <tamaabdy25@gmail.com>
2026-05-19 09:23:36 +0700

    Update koneksi.php

--- api/koneksi.php
+++ api/koneksi.php
@@ -9,32 +9,40 @@ if (!$conn) {
     die("Gagal konek ke database: " . pg_last_error());
 }
 
-// FUNGSI PENERJEMAH (Wrapper)
-// Ini biar kodingan PHP lu yang lama (mysqli_) tetep jalan di PostgreSQL
-function mysqli_query($conn, $query) {
-    return pg_query($conn, $query);
+// FUNGSI PENERJEMAH (Wrapper) dengan pengecekan function_exists
+if (!function_exists('mysqli_query')) {
+    function mysqli_query($conn, $query) {
+        return pg_query($conn, $query);
+    }
 }
 
-function mysqli_fetch_assoc($result) {
-    return pg_fetch_assoc($result);
+if (!function_exists('mysqli_fetch_assoc')) {
+    function mysqli_fetch_assoc($result) {
+        return pg_fetch_assoc($result);
+    }
 }
 
-function mysqli_num_rows($result) {
-    return pg_num_rows($result);
+if (!function_exists('mysqli_num_rows')) {
+    function mysqli_num_rows($result) {
+        return pg_num_rows($result);
+    }
 }
 
-function mysqli_real_escape_string($conn, $str) {
-    return pg_escape_string($conn, $str);
+if (!function_exists('mysqli_real_escape_string')) {
+    function mysqli_real_escape_string($conn, $str) {
+        return pg_escape_string($conn, $str);
+    }
 }
 
-function mysqli_error($conn) {
-    return pg_last_error($conn);
+if (!function_exists('mysqli_error')) {
+    function mysqli_error($conn) {
+        return pg_last_error($conn);
+    }
 }
 
-function mysqli_insert_id($conn) {
-    // PostgreSQL butuh cara khusus buat dapet ID terakhir, 
-    // tapi kalau lu pake wrapper ini, pastikan query INSERT lu di PHP 
-    // sudah diubah jadi '... RETURNING id'.
-    return null; 
+if (!function_exists('mysqli_insert_id')) {
+    function mysqli_insert_id($conn) {
+        return null; // Pastikan pakai 'RETURNING id' di query insert lu
+    }
 }
 ?>
