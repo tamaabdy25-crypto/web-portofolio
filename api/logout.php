8b94b65abfa8db113bdab74e2ac3489cdbf4885c
tamaabdy25-crypto <tamaabdy25@gmail.com>
2026-05-19 14:17:02 +0700

    Refactor logout.php to simplify session and cookie handling

--- api/logout.php
+++ api/logout.php
@@ -1,20 +1,12 @@
 <?php
 session_start();
+session_destroy(); // Hancurkan sesi
 
-// 1. Hapus semua data Session
-session_unset();
-session_destroy();
+// Hancurkan KTP (Cookie)
+setcookie('ingat_nomor_induk', '', time() - 3600, "/");
+setcookie('token_aman', '', time() - 3600, "/");
 
-// 2. HAPUS COOKIE (Biar gak Auto Login lagi)
-// Caranya: Set waktunya ke masa lalu (time - 3600)
-if (isset($_COOKIE['ingat_nomor_induk'])) {
-    setcookie('ingat_nomor_induk', '', time() - 3600, "/");
-}
-if (isset($_COOKIE['token_aman'])) {
-    setcookie('token_aman', '', time() - 3600, "/");
-}
-
-// 3. Lempar balik ke halaman login
+// Tendang balik ke login
 header("Location: login.php");
 exit;
 ?>
