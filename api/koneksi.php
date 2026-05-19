c1f1ecbcde08e952e0f9ede540132204661bb233
tamaabdy25-crypto <tamaabdy25@gmail.com>
2026-05-19 14:34:30 +0700

    Update koneksi.php

--- api/koneksi.php
+++ api/koneksi.php
@@ -31,4 +31,37 @@ if (!function_exists('mysqli_real_escape_string')) {
 if (!function_exists('mysqli_error')) {
     function mysqli_error($conn) { return pg_last_error($conn); }
 }
+    // ==================================================
+// GLOBAL VERCEL SESSION & COOKIE RESTORER
+// ==================================================
+// 1. Pastikan session menyala & aman di folder /tmp Vercel
+if (session_status() === PHP_SESSION_NONE) {
+    ini_set('session.save_path', '/tmp');
+    ini_set('session.cookie_path', '/');
+    ini_set('session.cookie_secure', 'On'); 
+    ini_set('session.cookie_httponly', 'On');
+    ini_set('session.cookie_samesite', 'Lax');
+    session_start();
+}
+
+// 2. BACA KTP (COOKIE) UNTUK SEMUA HALAMAN & API
+if (!isset($_SESSION['logged_in']) && isset($_COOKIE['ingat_nomor_induk']) && isset($_COOKIE['token_aman'])) {
+    $cookie_no_induk = pg_escape_string($conn, $_COOKIE['ingat_nomor_induk']);
+    $cookie_token    = $_COOKIE['token_aman'];
+    
+    // Cek database
+    if ($cookie_token === hash('sha256', $cookie_no_induk)) {
+        $q_cek_sesi = pg_query($conn, "SELECT * FROM users WHERE nomor_induk = '$cookie_no_induk'");
+        if (pg_num_rows($q_cek_sesi) > 0) {
+            $data_sesi = pg_fetch_assoc($q_cek_sesi);
+            
+            // Masukkan kembali ke ingatan (Session)
+            $_SESSION['logged_in']    = true;
+            $_SESSION['user_id']      = $data_sesi['id'];
+            $_SESSION['no_induk']     = $data_sesi['nomor_induk'];
+            $_SESSION['nama_lengkap'] = $data_sesi['nama_lengkap'];
+            $_SESSION['role']         = $data_sesi['role'];
+        }
+    }
+}
 ?>
